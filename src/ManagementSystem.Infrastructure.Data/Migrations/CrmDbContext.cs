1cd645081d60382490c53b18b305fcd252c341c9
Vitaliy Replyuk <vitaliy.replyuk@student.tuke.sk>
2024-03-02 17:43:31 +0000

    added DbContext

---
+++ src/ManagementSystem.Infrastructure.Data/Migrations/CrmDbContext.cs
@@ -0,0 +1,25 @@
+﻿using ManagementSystem.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Task = ManagementSystem.Core.Entities.Task;
+
+namespace ManagementSystem.Infrastructure.Data.Migrations
+{
+    public class CrmDbContext : DbContext
+    {
+        public CrmDbContext(DbContextOptions options) : base(options) { }
+        public CrmDbContext() { }
+        public DbSet<Case> Cases { get; set; }
+        public DbSet<Comment> Comments  { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<RecentSearch> RecentSearches { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CrmDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
